5224c5474ceeeb2cc05c40fe68158531badf7d95
tahmid4tune <tahmid.tee10@gmail.com>
2022-08-17 14:44:55 +0600

    Code cleanup

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { RatingsService } from './ratings.service';
 import { RatingDto } from './dto/rating.dto';
-import { UpdateRatingDto } from './dto/update-rating.dto';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { User } from '../users/entities/user.entity';
 
@@ -20,25 +11,5 @@ export class RatingsController {
   @Post()
   updateRating(@Body() ratingDto: RatingDto, @CurrentUser() user: User) {
     return this.ratingsService.updateRating(ratingDto, user);
-  }
-
-  @Get()
-  findAll() {
-    return this.ratingsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ratingsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRatingDto: UpdateRatingDto) {
-    return this.ratingsService.update(+id, updateRatingDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ratingsService.remove(+id);
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { Public } from '../auth/decorators/public.decorator';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { User } from './entities/user.entity';
@@ -23,21 +14,6 @@ export class UsersController {
   @Public()
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get('own-info')
-  findOne(@CurrentUser() user: User) {
-    return this.usersService.findOne(user);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete()

e644bc17c0e7f8ddae6cc3fe1b5300976df4792b
tahmid4tune <tahmid.tee10@gmail.com>
2022-08-25 21:36:57 +0600

    more unused imports

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -4,7 +4,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { plainToClass, plainToInstance } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import {
   Repository,
   Like,
